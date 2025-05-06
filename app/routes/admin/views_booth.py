d6db9a2537c6bd67bfa3bff79ebafa355de6cda6
unknown <tchdgolf@gmail.com>
2025-05-06 14:52:00 +0900

    Feat: Initial project setup and basic models (User, Pro, Booth)

--- app/routes/admin/views_booth.py
+++ app/routes/admin/views_booth.py
@@ -63,7 +63,7 @@ def edit_booth(booth_id):
         # GET 요청 시, 현재 타석 정보로 폼 필드 채우기
         form.name.data = booth.name
         # form.system_type.data = booth.system_type  # <<< 이 줄 추가!
-        form.system_type.data = booth.system_type.value # <<< Enum 멤버의 'value' (문자열)를 할당하도록 변경!
+        form.system_type.data = booth.system_type.name  # <<< Enum 멤버의 'value' (문자열)를 할당하도록 변경!
         form.is_available.data = booth.is_available
         form.memo.data = booth.memo
 
