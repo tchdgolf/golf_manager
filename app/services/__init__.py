a65dea503483171a779dd636ea34cf16bcf3f758
unknown <tchdgolf@gmail.com>
2025-05-06 17:28:06 +0900

    holding

---
+++ app/services/__init__.py
@@ -0,0 +1,4 @@
+# app/services/__init__.py
+from .holding_service import add_new_holding, delete_existing_holding, update_existing_holding
+# from .ticket_service import ... (나중에 추가)
+# from .user_service import ... (나중에 추가)
